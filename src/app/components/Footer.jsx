c05b643dfda54b6720f328d26e11264fa39a24d9
Prizag <priyanka060905@gmail.com>
2024-07-02 15:14:59 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,15 +1,34 @@
 "use client";
 
 import { ChevronUp } from "lucide-react";
+import Image from "next/image";
 
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-};
 
 const Footer = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.scrollY > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.scrollY <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop);
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ showScroll]);
   return (
     <>
       <link href="./output.css" rel="stylesheet"></link>
@@ -24,20 +43,23 @@ const Footer = () => {
           style={{ height: "550px" }}
         >
           <div className="container mx-auto text-center">
-            <img
-              src="./images/Drama-Logo (Navbar).png"
-              alt="Advay Logo"
-              className="mx-auto mb-2 w-34 h-32"
+           <Image
+            src="/images/Drama-Logo (Navbar).png"
+            alt="Advay Logo"
+            className="mx-auto mb-2 w-34 h-32"
+            width="300"
+            height="32"
             />
             <p>All Rights Reserved @Advay, NIT Silchar</p>
           </div>
+          {showScroll && (
           <button
             aria-label="Scroll to Top"
             onClick={scrollToTop}
             className="absolute bottom-2 right-4 text-white flex items-center justify-center p-3 rounded-full shadow-lg hover:bg-gray-600"
           >
             <ChevronUp size={55} strokeWidht={150} className="mt-1" />
-          </button>
+          </button>)}
         </footer>
       </div>
     </>
