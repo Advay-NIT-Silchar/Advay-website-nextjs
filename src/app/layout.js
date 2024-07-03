b015f229f4351758006eb9e2c3068adeca69486e
Prizag <priyanka060905@gmail.com>
2024-07-03 17:23:54 +0530

    Made Footer annd Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./globals.scss";
 
 
@@ -10,9 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         <Sidebar />
         
         {children}
+        <Footer/>
       </body>
     </html>
   );
