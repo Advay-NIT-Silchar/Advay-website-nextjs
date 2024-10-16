5de0e14be3e2cf12a2163dadde75c22c1df30eaf
Sahin Alam <sahin0945@gmail.com>
2024-10-16 09:13:58 +0530

    Sponsors section done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,17 @@
+import { Roboto } from "next/font/google";
+
+
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import "./globals.scss";
 
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: [ 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Advay NIT Silchar",
@@ -11,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className} >
         <Navbar/>
         <Sidebar />
         
