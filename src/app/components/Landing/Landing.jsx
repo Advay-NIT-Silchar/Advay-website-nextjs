f306d180279af07068d5fb41409fadce8b741dd8
Sahin Alam <sahin0945@gmail.com>
2024-10-17 01:24:59 +0530

    hero section complete

---
+++ src/app/components/Landing/Landing.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { Sixtyfour } from "next/font/google";
+import styles from "./Landing.module.scss";
+
+
+const sixty= Sixtyfour({
+    weight: ['400'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+const Landing = () => {
+  return (
+    <div className={`${styles.Landing} ${sixty.className}`}>
+        <Image src="/images/back.jpeg" fill alt="back" />
+      <div className={styles.leftCurtain}>
+        <Image src="/images/curtainLeft.png" fill alt="leftCurtain" />
+      </div>
+      <div className={styles.rightCurtain}>
+        <Image src="/images/curtainRight.png" fill alt="rightCurtain" />
+      </div>
+      <div className={styles.logo}>
+        <Image src="/images/Drama-Logo.webp" fill alt="logo" />
+      </div>
+      <div className={styles.title}>Coming soon
+        <span>.</span><span>.</span><span>.</span>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
